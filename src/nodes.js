9bcc3dca5a8efc061d15863638c51d228a93eb8d
Dharmendra Singh Negi <dharmenn@thoughtworks.com>
2016-11-22 13:14:12 +0530

    Implemented OperatorNode and NumberNode

--- src/nodes.js
+++ src/nodes.js
@@ -1,4 +1,13 @@
 var nodes = {};
+var operators = {};
+
+nodes.NumberNode = function (value) {
+    this.value = value;
+    this.evaluate = function () {
+        return this.value;
+    };
+};
+
 
 nodes.AssignmentNode = function (value) {
     this.value = value;
@@ -6,18 +15,72 @@ nodes.AssignmentNode = function (value) {
 };
 
 nodes.AssignmentNode.prototype = {
-    insertChildren: function (child) {
+    insertChild: function (child) {
         this.children.push(child);
     },
     evaluate: function (assignmentTable) {
         var child = this.children[0];
-        if(child instanceof nodes.AssignmentNode) {
+        if (child instanceof nodes.AssignmentNode) {
             child.evaluate(assignmentTable);
             assignmentTable[this.value] = child.value;
         }
         else
             assignmentTable[this.value] = child.evaluate(assignmentTable);
     }
+};
+
+nodes.OperatorNode = function (value) {
+    this.value = value;
+    this.children = [];
+};
+
+nodes.OperatorNode.prototype = {
+    insertChild: function (child) {
+        this.children.push(child);
+    },
+    evaluate: function () {
+        return operators[this.value].apply(this).value;
+    }
+};
+
+operators = {
+
+    '+': function () {
+        return this.children.reduce(function (prevVal, currentVal) {
+            var out = prevVal.evaluate() + currentVal.evaluate();
+            return new nodes.NumberNode(out);
+        });
+    },
+
+    '-': function () {
+        return this.children.reduce(function (prevVal, currentVal) {
+            var out = prevVal.evaluate() - currentVal.evaluate();
+            return new nodes.NumberNode(out);
+        });
+    },
+
+    '*': function () {
+        return this.children.reduce(function (prevVal, currentVal) {
+            var out = prevVal.evaluate() * currentVal.evaluate();
+            return new nodes.NumberNode(out);
+        });
+    },
+
+    '/': function () {
+        return this.children.reduce(function (prevVal, currentVal) {
+            var out = prevVal.evaluate() / currentVal.evaluate();
+            return new nodes.NumberNode(out);
+        });
+    },
+
+    '^': function () {
+        var reversedValues = this.children.reverse();
+        return reversedValues.reduce(function (prevVal, currentVal) {
+            var out = Math.pow(currentVal.evaluate(),prevVal.evaluate());
+            return new nodes.NumberNode(out);
+        });
+    }
+
 };
 
 module.exports = nodes;
--- test/nodesTest.js
+++ test/nodesTest.js
@@ -1,37 +1,108 @@
 var expect = require('unexpected');
 
 var AssignmentNode = require('../src/nodes.js').AssignmentNode;
+var OperatorNode = require('../src/nodes.js').OperatorNode;
+var NumberNode = require('../src/nodes.js').NumberNode;
 
-var NumberNode = function (value) {
-    this.value = value;
-    this.evaluate = function () {
-        return this.value;
-    };
-};
+describe('Nodes', function () {
+    describe('AssignmentNode', function () {
 
-describe('AssignmentNode', function () {
-    var assignmentTable;
-    var assignmentNode;
-    beforeEach(function () {
-        assignmentTable = {};
-        assignmentNode = new AssignmentNode('a');
+        var assignmentTable;
+        var assignmentNode;
+        beforeEach(function () {
+            assignmentTable = {};
+            assignmentNode = new AssignmentNode('a');
+        });
+
+        it('should evaluate it self and populate the assignment table', function () {
+            assignmentNode.insertChild(new NumberNode(4));
+            assignmentNode.evaluate(assignmentTable);
+
+            var expectedAssignmentTable = {a: 4};
+            expect(assignmentTable, 'to equal', expectedAssignmentTable);
+        });
+
+        it('should evaluate it self when children are of AssignmentNode type', function () {
+            var assignmentNode2 = new AssignmentNode('b');
+            assignmentNode2.insertChild(new NumberNode(4));
+            assignmentNode.insertChild(assignmentNode2);
+            assignmentNode.evaluate(assignmentTable);
+
+            var expectedAssignmentTable = {a: 'b', b: 4};
+            expect(assignmentTable, 'to equal', expectedAssignmentTable);
+        });
     });
 
-    it('should evaluate it self and populate the assignment table', function () {
-        assignmentNode.insertChildren(new NumberNode(4));
-        assignmentNode.evaluate(assignmentTable);
+    describe('OperatorNode', function () {
 
-        var expectedAssignmentTable = {a: 4};
-        expect(assignmentTable, 'to equal', expectedAssignmentTable);
+        describe(' + operator', function () {
+
+            var assignmentTable;
+            var operatorNode;
+            beforeEach(function () {
+                assignmentTable = {};
+                operatorNode = new OperatorNode('+');
+
+            });
+
+            it('should evaluate it self', function () {
+                operatorNode.insertChild(new NumberNode(4));
+                operatorNode.insertChild(new NumberNode(5));
+
+                expect(operatorNode.evaluate(), 'to equal', 9);
+            });
+
+        });
+
+        describe(' - operator', function () {
+
+            it('should evaluate it self', function () {
+                var operatorNode = new OperatorNode('-');
+                operatorNode.insertChild(new NumberNode(4));
+                operatorNode.insertChild(new NumberNode(5));
+
+                expect(operatorNode.evaluate(), 'to equal', -1);
+            });
+
+        });
+
+        describe(' * operator', function () {
+
+            it('should evaluate it self', function () {
+                var operatorNode = new OperatorNode('*');
+                operatorNode.insertChild(new NumberNode(4));
+                operatorNode.insertChild(new NumberNode(5));
+
+                expect(operatorNode.evaluate(), 'to equal', 20);
+            });
+
+        });
+
+        describe(' / operator', function () {
+
+            it('should evaluate it self', function () {
+                var operatorNode = new OperatorNode('/');
+                operatorNode.insertChild(new NumberNode(40));
+                operatorNode.insertChild(new NumberNode(20));
+                operatorNode.insertChild(new NumberNode(2));
+
+                expect(operatorNode.evaluate(), 'to equal', 1);
+            });
+
+        });
+
+        describe(' ^ operator', function () {
+
+            it('should evaluate it self', function () {
+                var operatorNode = new OperatorNode('^');
+                operatorNode.insertChild(new NumberNode(3));
+                operatorNode.insertChild(new NumberNode(3));
+                operatorNode.insertChild(new NumberNode(2));
+
+                expect(operatorNode.evaluate(), 'to equal', 19683);
+            });
+
+        });
     });
 
-    it('should evaluate it self when children are of AssignmentNode type', function () {
-        var assignmentNode2 = new AssignmentNode('b');
-        assignmentNode2.insertChildren(new NumberNode(4));
-        assignmentNode.insertChildren(assignmentNode2);
-        assignmentNode.evaluate(assignmentTable);
-
-        var expectedAssignmentTable = {a: 'b', b: 4};
-        expect(assignmentTable, 'to equal', expectedAssignmentTable);
-    });
 });

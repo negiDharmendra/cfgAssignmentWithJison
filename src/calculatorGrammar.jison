01dc799af898731b1e6afe99e0725b48f5ba159b
Dharmendra Singh Negi <dharmenn@thoughtworks.com>
2016-11-23 14:48:44 +0530

    Refactoring calculator grammar

--- src/calculatorGrammar.jison
+++ src/calculatorGrammar.jison
@@ -3,8 +3,16 @@
 /* description: Parses and executes mathematical expressions. */
 
 %{
+
+var path = require('path');
+var AssignmentNode = require(path.resolve('./src/nodes.js')).AssignmentNode;
+var OperatorNode = require(path.resolve('./src/nodes.js')).OperatorNode;
+var NumberNode = require(path.resolve('./src/nodes.js')).NumberNode;
+var IdentifierNode = require(path.resolve('./src/nodes.js')).IdentifierNode;
+
+
     function combineExpListAndExp($1,$2){
-        if($2[0].constructor == Array){
+        if($2.constructor == Array){
             $2.unshift($1);
             return $2
         }
@@ -49,14 +57,14 @@
 
 expressions
     : expression EOF
-        {   return $1}
+        { return $1 }
     ;
 
 expression
     : mExpression TERMINATOR
     | declaration
     | declaration expression
-        {   $$  = combineExpListAndExp($1,$2) }
+        {  $$  = combineExpListAndExp($1,$2) }
     ;
 
 declaration
@@ -65,30 +73,60 @@ declaration
 
 assignment
     : identifier '='  mExpression TERMINATOR
-       {$$ = [$2,$1,$3];}
+       {
+           var assignmentNode = new AssignmentNode($2);
+           assignmentNode.insertChild($1);
+           assignmentNode.insertChild($3);
+           $$ = assignmentNode;
+       }
     ;
 
 mExpression
     : mExpression '+' mExpression
-        {$$ = [$2,$1,$3];}
+        {
+            var operatorNode = new OperatorNode($2);
+            operatorNode.insertChild($1);
+            operatorNode.insertChild($3);
+            $$ = operatorNode;
+        }
     | mExpression '*' mExpression
-        {$$ = [$2,$1,$3];}
+        {
+            var operatorNode = new OperatorNode($2);
+            operatorNode.insertChild($1);
+            operatorNode.insertChild($3);
+            $$ = operatorNode;
+        }
     | mExpression '-' mExpression
-        {$$ = [$2,$1,$3];}
+       {
+           var operatorNode = new OperatorNode($2);
+           operatorNode.insertChild($1);
+           operatorNode.insertChild($3);
+           $$ = operatorNode;
+       }
     | powerExpression
     |identifier
     | NUMBER
-        {$$ = Number(yytext)}
+        {$$ = new NumberNode(Number(yytext));}
     ;
 
 powerExpression
     : '(' mExpression '^' mExpression ')'
-            {$$ = [$3,$2,$4];}
+            {
+                var operatorNode = new OperatorNode($3);
+                operatorNode.insertChild($2);
+                operatorNode.insertChild($4);
+                $$ = operatorNode;
+            }
     | mExpression '^' mExpression
-            {$$ = [$2,$1,$3];}
+           {
+               var operatorNode = new OperatorNode($2);
+               operatorNode.insertChild($1);
+               operatorNode.insertChild($3);
+               $$ = operatorNode;
+           }
     ;
 
 identifier
     : IDENTIFIER
-        {$$ = yytext}
+        {$$ = new IdentifierNode(yytext);}
     ;
--- test/calculatorGrammarTest.js
+++ test/calculatorGrammarTest.js
@@ -1,108 +1,200 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 var fs = require('fs');
+
+var AssignmentNode = require('../src/nodes.js').AssignmentNode;
+var OperatorNode = require('../src/nodes.js').OperatorNode;
+var NumberNode = require('../src/nodes.js').NumberNode;
+var IdentifierNode = require('../src/nodes.js').IdentifierNode;
+
 var grammar = fs.readFileSync('./src/calculatorGrammar.jison', 'utf-8');
 
 var Parser = require('jison').Parser;
 var parser = new Parser(grammar);
 
 describe('calculator grammar', function () {
-        it("should represent 1+2 as ['+',1,2]", function () {
-            var actual = parser.parse('1+2;');
-            var expected = ['+', 1, 2];
-            assert.deepEqual(actual, expected)
-        });
+    it("should represent 1+2 as ['+',1,2]", function () {
+        var actual = parser.parse('1+2;');
+        var operatorNode = new OperatorNode('+');
+        operatorNode.insertChild(new NumberNode(1));
+        operatorNode.insertChild(new NumberNode(2));
+        var expected = operatorNode;
+        assert.deepEqual(actual, expected);
+    });
 
-        it("should represent 1+2+3 as ['+', ['+', 1, 2], 3]", function () {
-            var actual = parser.parse('1+2+3;');
-            var expected = ['+', ['+', 1, 2], 3];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent 1+2+3 as ['+', ['+', 1, 2], 3]", function () {
+        var actual = parser.parse('1+2+3;');
 
-        it("should represent 1*2 as ['*', 1, 2]", function () {
-            var actual = parser.parse('1*2;');
-            var expected = ['*', 1, 2];
-            assert.deepEqual(actual, expected);
-        });
+        var childOperatorNode = new OperatorNode('+');
+        childOperatorNode.insertChild(new NumberNode(1));
+        childOperatorNode.insertChild(new NumberNode(2));
 
-        it("should represent 1*2*3 as ['*', ['*', 1, 2], 3]", function () {
-            var actual = parser.parse('1*2*3;');
-            var expected = ['*', ['*', 1, 2], 3];
-            assert.deepEqual(actual, expected);
-        });
+        var rootOperatorNode = new OperatorNode('+');
+        rootOperatorNode.insertChild(childOperatorNode);
+        rootOperatorNode.insertChild(new NumberNode(3));
+
+        var expected = rootOperatorNode;
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it("should represent 1*2 as ['*', 1, 2]", function () {
+        var actual = parser.parse('1*2;');
+        var operatorNode = new OperatorNode('*');
+        operatorNode.insertChild(new NumberNode(1));
+        operatorNode.insertChild(new NumberNode(2));
+        var expected = operatorNode;
+        assert.deepEqual(actual, expected);
+    });
+
+    it("should represent 1*2*3 as ['*', ['*', 1, 2], 3]", function () {
+        var actual = parser.parse('1*2*3;');
+
+        var childOperatorNode = new OperatorNode('*');
+        childOperatorNode.insertChild(new NumberNode(1));
+        childOperatorNode.insertChild(new NumberNode(2));
+
+        var rootOperatorNode = new OperatorNode('*');
+        rootOperatorNode.insertChild(childOperatorNode);
+        rootOperatorNode.insertChild(new NumberNode(3));
+
+        var expected = rootOperatorNode;
+
+        assert.deepEqual(actual, expected);
+    });
 
 
-        it("should represent 1+2*3 as ['+', 1, ['*', 2, 3]]", function () {
-            var actual = parser.parse('1+2*3;');
-            var expected = ['+', 1, ['*', 2, 3]];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent 1+2*3 as ['+', 1, ['*', 2, 3]]", function () {
+        var actual = parser.parse('1+2*3;');
+
+        var childOperatorNode = new OperatorNode('*');
+        childOperatorNode.insertChild(new NumberNode(2));
+        childOperatorNode.insertChild(new NumberNode(3));
+
+        var rootOperatorNode = new OperatorNode('+');
+        rootOperatorNode.insertChild(new NumberNode(1));
+        rootOperatorNode.insertChild(childOperatorNode);
+
+        var expected = rootOperatorNode;
+
+        assert.deepEqual(actual, expected);
+    });
 
 
-        it("should represent 1000000000*2000000 as ['*', 1000000000, 2000000]", function () {
-            var actual = parser.parse('1000000000*2000000;');
-            var expected = ['*', 1000000000, 2000000];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent 1000000000*2000000 as ['*', 1000000000, 2000000]", function () {
+        var actual = parser.parse('1000000000*2000000;');
 
-        it("should represent 3^2; as ['^', 3, 2]", function () {
-            var actual = parser.parse('3^2;');
-            var expected = ['^', 3, 2];
-            assert.deepEqual(actual, expected);
-        });
+        var operatorNode = new OperatorNode('*');
+        operatorNode.insertChild(new NumberNode(1000000000));
+        operatorNode.insertChild(new NumberNode(2000000));
+        var expected = operatorNode;
+        assert.deepEqual(actual, expected);
 
-        it("should represent a=2; as ['=', 'a', 2]", function () {
-            var actual = parser.parse('a=2;');
-            var expected = ['=', 'a', 2];
-            assert.deepEqual(actual, expected);
-        });
+        assert.deepEqual(actual, expected);
+    });
 
-        it("should represent a=5;2+4 as [['=', 'a', 2],['+',4,5]]", function () {
-            var actual = parser.parse('a=5;2+4;');
-            var expected = [['=', 'a', 5],['+',2,4]];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent 3^2; as ['^', 3, 2]", function () {
+        var actual = parser.parse('3^2;');
 
-        it("should represent a=5;a+4 as [['=', 'a', 2],['+',a,5]]", function () {
-            var actual = parser.parse('a=5;a+4;');
-            var expected = [['=', 'a', 5],['+','a',4]];
-            assert.deepEqual(actual, expected);
-        });
+        var operatorNode = new OperatorNode('^');
+        operatorNode.insertChild(new NumberNode(3));
+        operatorNode.insertChild(new NumberNode(2));
+        var expected = operatorNode;
 
-        it.skip("should represent a=v=2; as ['=', 'a', ['=', 'v', 2]]", function () {
-            var actual = parser.parse('a=v=2;');
-            var expected = ['=', 'a', ['=', 'v', 2]];
-            assert.deepEqual(actual, expected);
-        });
+        assert.deepEqual(actual, expected);
+    });
 
-        it.skip("should represent a=v=c=2; as ['=', 'a', ['=', 'v', 2]]", function () {
-            var actual = parser.parse('a=v=c=2;');
-            var expected = ['=', 'a', ['=', 'v', ['=','c',2]]];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent a=2; as ['=', 'a', 2]", function () {
+        var actual = parser.parse('a=2;');
+        var assignmentNode = new AssignmentNode('=');
+        assignmentNode.insertChild(new IdentifierNode('a'));
+        assignmentNode.insertChild(new NumberNode(2));
+        assert.deepEqual(actual, assignmentNode);
+    });
 
-        it("should represent a=3;b=2; as [['=', 'a', 3],['=','b',2]]", function () {
-            var actual = parser.parse('a=3;b=2;');
-            var expected = [['=', 'a', 3],['=','b',2]];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent a=5;2+4 as [['=', 'a', 2],['+',4,5]]", function () {
+        var actual = parser.parse('a=5;2+4;');
+        var assignmentNode = new AssignmentNode('=');
+        assignmentNode.insertChild(new IdentifierNode('a'));
+        assignmentNode.insertChild(new NumberNode(5));
 
-        it.skip("should represent a=c=3;b=2; as [['=', 'a', 3],['=','b',2]]", function () {
-            var actual = parser.parse('a=c=3;b=2;');
-            var expected = [['=', 'a', ['=', 'c', 3]],['=','b',2]];
-            assert.deepEqual(actual, expected);
-        });
+        var operatorNode = new OperatorNode('+');
+        operatorNode.insertChild(new NumberNode(2));
+        operatorNode.insertChild(new NumberNode(4));
 
-        it("should represent a=2+3; as ['=', 'a', ['+', 2, 3]]", function () {
-            var actual = parser.parse('a=2+3;');
-            var expected = ['=', 'a', ['+', 2, 3]];
-            assert.deepEqual(actual, expected);
-        });
+        var expected = [assignmentNode, operatorNode];
+        assert.deepEqual(actual, expected);
+    });
 
-        it("should be able to generate tree for a=23+23;f=4+4;d=4;d=4+45;", function () {
-            var actual = parser.parse('a=23+23;f=4+4;d=4;d=4+45;');
-            var expected = [ [ '=', 'a', [ '+', 23, 23 ] ],[ '=', 'f', [ '+', 4, 4 ] ],[ '=', 'd', 4 ],[ '=', 'd', [ '+', 4, 45 ] ] ];
-            assert.deepEqual(actual, expected);
-        });
+    it("should represent a=5;a+4 as [['=', 'a', 2],['+',a,5]]", function () {
+        var actual = parser.parse('a=5;a+4;');
+
+        var assignmentNode = new AssignmentNode('=');
+        assignmentNode.insertChild(new IdentifierNode('a'));
+        assignmentNode.insertChild(new NumberNode(5));
+
+        var operatorNode = new OperatorNode('+');
+        operatorNode.insertChild(new IdentifierNode('a'));
+        operatorNode.insertChild(new NumberNode(4));
+
+        var expected = [assignmentNode, operatorNode];
+        assert.deepEqual(actual, expected);
+    });
+
+    it("should represent a=3;b=2; as [['=', 'a', 3],['=','b',2]]", function () {
+        var actual = parser.parse('a=3;b=2;');
+
+        var assignmentNode = new AssignmentNode('=');
+        assignmentNode.insertChild(new IdentifierNode('a'));
+        assignmentNode.insertChild(new NumberNode(3));
+
+        var secondAssignmentNode = new AssignmentNode('=');
+        secondAssignmentNode.insertChild(new IdentifierNode('b'));
+        secondAssignmentNode.insertChild(new NumberNode(2));
+
+        var expected = [assignmentNode, secondAssignmentNode];
+        assert.deepEqual(actual, expected);
+    });
+
+    it("should represent a=2+3; as ['=', 'a', ['+', 2, 3]]", function () {
+        var actual = parser.parse('a=2+3;');
+
+        var operatorNode = new OperatorNode('+');
+        operatorNode.insertChild(new NumberNode(2));
+        operatorNode.insertChild(new NumberNode(3));
+
+        var expectedNode = new AssignmentNode('=');
+        expectedNode.insertChild(new IdentifierNode('a'));
+        expectedNode.insertChild(operatorNode);
+
+        assert.deepEqual(actual, expectedNode);
+    });
+
+    it("should be able to generate tree for a=23+23;f=4+4;d=4;d=4+45;", function () {
+        var actual = parser.parse('a=23+23;f=23+23;d=4;23+23;');
+
+        var operatorNode = new OperatorNode('+');
+        operatorNode.insertChild(new NumberNode(23));
+        operatorNode.insertChild(new NumberNode(23));
+
+        var assignmentNodeA = new AssignmentNode('=');
+        assignmentNodeA.insertChild(new IdentifierNode('a'));
+        assignmentNodeA.insertChild(operatorNode);
+
+
+        var assignmentNodeB = new AssignmentNode('=');
+        assignmentNodeB.insertChild(new IdentifierNode('f'));
+        assignmentNodeB.insertChild(operatorNode);
+
+
+        var assignmentNodeD = new AssignmentNode('=');
+        assignmentNodeD.insertChild(new IdentifierNode('d'));
+        assignmentNodeD.insertChild(new NumberNode(4));
+
+        var expected = [assignmentNodeA, assignmentNodeB, assignmentNodeD, operatorNode];
+        assert.deepEqual(actual, expected);
+    });
+
+
 });
 
